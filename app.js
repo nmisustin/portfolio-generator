b332ae4ed86e66ecd03f3b6da9e315c27796f680
nmisustin <nmisustin@gmail.com>
2021-04-09 22:44:41 -0700

    added way to get info from the command line

---
+++ app.js
@@ -0,0 +1,9 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const printProfileData = (profileDataArr) => {
+    for (let i=0; i<profileDataArr.length; i++){
+        console.log(profileDataArr[i]);
+    }
+    //is the same as
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+printProfileData(profileDataArgs);
